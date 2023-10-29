7cea9d4b3f7c6671845d3c8cd2fecf3242fb7c8b
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-29 17:05:53 -0400

    wip

--- project-1/icat.nix
+++ project-1/icat.nix
@@ -14,5 +14,7 @@ stdenv.mkDerivation {
     sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka";
     # nix-prefetch-url --unpack https://github.com/atextor/icat/archive/refs/tags/v0.5.tar.gz --type sha256
   };
+
+  buildInputs = [ imlib2 ];
 }
 

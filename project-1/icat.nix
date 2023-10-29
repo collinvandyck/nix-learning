fa95ab412b05bc331e2f460888429b8e8236cf78
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-29 17:03:35 -0400

    wip

---
+++ project-1/icat.nix
@@ -0,0 +1,16 @@
+# icat.nix
+{ lib
+, stdenv 
+, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  name = "icat";
+  src = fetchFromGitHub {
+    owner = "atextor";
+    repo = "icat";
+    rev = "v0.5";
+    sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka";
+    # nix-prefetch-url --unpack https://github.com/atextor/icat/archive/refs/tags/v0.5.tar.gz --type sha256
+  };
+}
+

0c892d604d833a6c6fe3d1264891cc70d771b3e0
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-29 11:12:24 -0400

    wip

--- basics/functions-1/default.nix
+++ basics/functions-1/default.nix
@@ -3,10 +3,12 @@ let
   add = x: y: x + y;
   mul = x: y: x * y;
   att_add = { a, b }: a + b;
+  args_add = args@{ a, b, ... }: a + b + args.c;
 in
   {
     x = ident(5);
     x2 = add(5)(4);
     x3 = mul(5)(4);
     x4 = att_add({ a=1; b=2; });
+    x5 = args_add({ a=1; b=2; c=3; });
   }
